5ecd0cec02d99233fe8831a014fdce8ffc697049
cristina <adp.cris@gmail.com>
2014-09-06 12:14:19 +0100

    installation app

--- www/js/main.js
+++ www/js/main.js
@@ -1,33 +1,27 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        console.log(this);
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+           $('.employee-list').html(self.employeeLiTpl(employees));
+            console.log(this);
         });
     },
 
-/* the next function is commented to keep it clean and use the following one to test the alert messages */
-/*
-    initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
-*/
     initialize: function() {
 
         var self = this;/* added */
         this.store = new MemoryStore(function(){
-            self.showAlert('Yeah man the Store is Initialized', 'Info');/* added */
+            /* we don't need an alert to be shown every time, so for now we'll comment this bit */
+            //self.showAlert('Yeah man the Store is Initialized', 'Info');/* added */
+
+            /* we compile the templates defined in the index.html */
+            this.homeTpl = Handlebars.compile($('#home-tpl').html());
+            this.employeeLiTpl = Handlebars.compile($('#employee-li-tpl').html());
+            /* we call the function to render the html */
+            self.renderHomeView();
+
         });
 
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
@@ -39,6 +33,16 @@ var app = {
         } else {
             alert(title ? (title + ": " + message) : message);
         }
+    },
+
+    /* We render the html through the controller instead of the html itself in order
+     * to create a SINGLE PAGE APPLICATION which provides a more fluid experience for
+     * the user and the possibility of using it off-line */
+    renderHomeView: function(){
+        $('body').html(this.homeTpl());
+        /* the following line is the one that actually calls the one that makes all
+         the work of looking for the words that the user's typing in the input */
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
